7e31a32acd3120f9d34238c5451abad213084d4b
Adithya Nair <adtya@adtya.xyz>
2023-03-20 21:24:56 +0530

    add tracker to aria2

--- home/downloader.nix
+++ home/downloader.nix
@@ -3,8 +3,8 @@
     aria2 = {
       enable = true;
       settings = {
+        bt-tracker = "udp://tracker.opentrackr.org:1337/announce";
         continue = true;
-        file-allocation = "falloc";
         max-connection-per-server = 8;
         split = 8;
         min-split-size = "1M";
