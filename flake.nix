9e41dbfa7ee6ec5f1521e2b899dc55699a85554c
Adithya Nair <adtya@adtya.xyz>
2023-04-02 00:30:38 +0530

    remove explicit branch names

--- flake.nix
+++ flake.nix
@@ -7,21 +7,21 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     impermanence = {
-      url = "github:nix-community/impermanence/master";
+      url = "github:nix-community/impermanence";
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/master";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:pta2002/nixvim/main";
+      url = "github:pta2002/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
