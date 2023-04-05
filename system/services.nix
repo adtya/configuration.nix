73bbbb7ffd359719e7bac09d9a2f3a002c83e966
Adithya Nair <adtya@adtya.xyz>
2023-04-06 02:39:16 +0530

    setup tlp

--- system/services.nix
+++ system/services.nix
@@ -21,6 +21,22 @@
     power-profiles-daemon.enable = true;
     resolved.enable = true;
     thermald.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC="performance";
+        CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC="performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT="power";
+
+        CPU_MAX_PERF_ON_AC=100;
+        CPU_MAX_PERF_ON_BAT=60;
+
+        CPU_BOOST_ON_AC=1;
+        CPU_BOOST_ON_BAT=0;
+      };
+    };
     udev.packages = [ pkgs.yubikey-personalization ];
     udisks2.enable = true;
   };
