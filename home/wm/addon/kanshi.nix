081cc212890742a8d4be2803d2530ceb27df7627
Adithya Nair <adtya@adtya.xyz>
2023-04-07 22:58:35 +0530

    handle external outputs on different ports

--- home/wm/addon/kanshi.nix
+++ home/wm/addon/kanshi.nix
@@ -11,10 +11,27 @@
           }
         ];
       };
-      docked = {
+      docked_1 = {
         outputs = [
           {
             criteria = "eDP-1";
+            transform = "normal";
+            mode = "1920x1080";
+            position = "0,216";
+            scale = 1.25;
+          }
+          {
+            criteria = "DP-1";
+            transform = "normal";
+            mode = "1920x1080";
+            position = "1536,0";
+          }
+        ];
+      };
+      docked_2 = {
+        outputs = [
+          {
+            criteria = "eDP-2";
             transform = "normal";
             mode = "1920x1080";
             position = "0,216";
