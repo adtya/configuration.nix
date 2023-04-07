8dcf43e4e0db25deff8feccc9b97befa79f88793
Adithya Nair <adtya@adtya.xyz>
2023-04-07 19:52:41 +0530

    cleanup

--- system/hardware/default.nix
+++ system/hardware/default.nix
@@ -1,12 +1,7 @@
-{ lib
-, pkgs
-, config
-, ...
-}: {
+{ lib, pkgs, config, ... }: {
   imports = [ ./kernel.nix ./filesystem.nix ];
 
   boot = {
-    resumeDevice = "/dev/vg0/swap";
     initrd.luks.devices = {
       luks0 = {
         allowDiscards = true;
@@ -15,38 +10,34 @@
         preLVM = true;
       };
     };
+    loader.efi.canTouchEfiVariables = true;
+    resumeDevice = "/dev/vg0/swap";
   };
 
   swapDevices = [{ device = "/dev/vg0/swap"; }];
 
-  hardware.opengl = {
-    enable = true;
-    extraPackages = [ pkgs.intel-media-driver ];
-    driSupport = true;
-  };
-
   hardware = {
+    bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Experimental = true;
+          KernelExperimental = true;
+        };
+      };
+      package = (pkgs.bluez.override { withExperimental = true; });
+    };
     cpu.intel.updateMicrocode = lib.mkDefault true;
     enableRedistributableFirmware = true;
-  };
-
-  hardware.bluetooth = {
-    settings = {
-      General = {
-        Experimental = true;
-        KernelExperimental = true;
-      };
+    gpgSmartcards.enable = true;
+    opengl = {
+      enable = true;
+      extraPackages = [ pkgs.intel-media-driver ];
+      driSupport = true;
     };
-    package = (pkgs.bluez.override { withExperimental = true; });
-    enable = true;
-  };
-
-  hardware.sensor.hddtemp = {
-    enable = true;
-    drives = [ "/dev/disk/by-path/pci-0000:01:00.0-nvme-1" ];
-  };
-
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
+    sensor.hddtemp = {
+      enable = true;
+      drives = [ "/dev/disk/by-path/pci-0000:01:00.0-nvme-1" ];
+    };
   };
 }
