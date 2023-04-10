a87c54d6ab0044e66f4680331279bf3b978b39f2
Adithya Nair <adtya@adtya.xyz>
2023-04-10 23:46:46 +0530

    setup security stuff

--- system.rico/security.nix
+++ system.rico/security.nix
@@ -1,3 +1,15 @@
 { ... }: {
-  security.sudo.wheelNeedsPassword = false;
+  security = {
+    apparmor = {
+      enable = true;
+      enableCache = true;
+    };
+    audit.enable = true;
+    auditd.enable = true;
+    sudo = {
+      wheelNeedsPassword = false;
+    };
+    polkit.enable = true;
+    rtkit.enable = true;
+  };
 }
