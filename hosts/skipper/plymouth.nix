ecde9184c3fab1c25e67a82925d801c17b2c0cc1
Adithya Nair <adtya@adtya.xyz>
2023-05-20 21:14:54 +0530

    switch to rog

--- hosts/skipper/plymouth.nix
+++ hosts/skipper/plymouth.nix
@@ -3,8 +3,8 @@
     enable = true;
     themePackages = [
       (pkgs.adi1090x-plymouth.override {
-        pack = "pack_3";
-        theme = "owl";
+        pack = "pack_4";
+        theme = "rog_2";
       })
     ];
     theme = "adi1090x";
