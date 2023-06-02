9278a68e50eb8367e7374461274fbcfc09e683bd
Adithya Nair <adtya@adtya.xyz>
2023-06-03 00:43:12 +0530

    fmt

--- hosts/rico2/containers/adtya.xyz.nix
+++ hosts/rico2/containers/adtya.xyz.nix
@@ -10,16 +10,16 @@
       reverse_proxy http://127.0.0.1:3000
 
       handle /.well-known/matrix/server {
-                header Content-Type application/json
-                header Access-Control-Allow-Origin *
-                respond `{"m.server": "matrix.adtya.xyz:443"}`
-        }
+        header Content-Type application/json
+        header Access-Control-Allow-Origin *
+        respond `{"m.server": "matrix.adtya.xyz:443"}`
+      }
 
-        handle /.well-known/matrix/client {
-                header Content-Type application/json
-                header Access-Control-Allow-Origin *
-                respond `{"m.homeserver": {"base_url": "https://matrix.adtya.xyz:443"}}`
-        }
+      handle /.well-known/matrix/client {
+        header Content-Type application/json
+        header Access-Control-Allow-Origin *
+        respond `{"m.homeserver": {"base_url": "https://matrix.adtya.xyz:443"}}`
+      }
     '';
   };
 }
