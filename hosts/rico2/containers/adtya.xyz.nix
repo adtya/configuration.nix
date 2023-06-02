b4c68696081bd7ad06784f79a40125cb302a96e6
Adithya Nair <adtya@adtya.xyz>
2023-06-02 13:35:52 +0530

    rico2: setup matrix well-knowns

--- hosts/rico2/containers/adtya.xyz.nix
+++ hosts/rico2/containers/adtya.xyz.nix
@@ -9,6 +9,18 @@
     serverAliases = ["www.adtya.xyz"];
     extraConfig = ''
       reverse_proxy http://127.0.0.1:3000
+
+      handle /.well-known/matrix/server {
+                header Content-Type application/json
+                header Access-Control-Allow-Origin *
+                respond `{"m.server": "matrix.adtya.xyz:443"}`
+        }
+
+        handle /.well-known/matrix/client {
+                header Content-Type application/json
+                header Access-Control-Allow-Origin *
+                respond `{"m.homeserver": {"base_url": "https://matrix.adtya.xyz:443"}}`
+        }
     '';
   };
 }
