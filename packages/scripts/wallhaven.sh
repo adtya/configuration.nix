259c8bb5a0e5c2fdc53478a756ab213d1dce9e49
Adithya Nair <adtya@adtya.xyz>
2023-07-08 14:10:55 +0530

    4k wallpapers ftw

--- packages/scripts/wallhaven.sh
+++ packages/scripts/wallhaven.sh
@@ -6,7 +6,7 @@ DIR="${1:-/tmp}"
 mkdir -p "$DIR"
 
 notify-send -r 9897 -i information -t 1000 "Wallpapers" "Downloading..."
-URL="https://wallhaven.cc/api/v1/search?categories=100&purity=100&atleast=1920x1080&ratios=16x9&sorting=random"
+URL="https://wallhaven.cc/api/v1/search?categories=100&purity=100&atleast=3840x2160&ratios=16x9&sorting=random"
 ID="$(curl --silent "$URL" | jq -r '.data[0].id')"
 IMAGE_META=$(curl --silent "https://wallhaven.cc/api/v1/w/$ID")
 IMAGE_URL="$(echo "$IMAGE_META" | jq -r '.data.path')"
