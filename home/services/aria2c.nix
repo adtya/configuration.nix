6d13645d29caeeaa68553dfeac88c5676ff4dc35
Adithya Nair <adtya@adtya.xyz>
2023-07-09 22:14:29 +0530

    create user service for ariang

--- home/services/aria2c.nix
+++ home/services/aria2c.nix
@@ -4,20 +4,37 @@
   secrets,
   ...
 }: {
-  systemd.user.services.aria2c = {
-    Unit = {
-      Description = "Aria2c Download Manager";
-      Documentation = ["man:aria2c(1)"];
-      After = ["network.target"];
+  systemd.user.services = {
+    ariang = {
+      Unit = {
+        Description = "AriaNg: Web frontend for aria2c";
+        After = ["network.target" "aria2c.service"];
+      };
+      Install = {
+        WantedBy = ["default.target"];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.merecat}/bin/merecat -n -p 6801 "${pkgs.ariang}/share/ariang"
+        '';
+      };
     };
-    Install = {
-      WantedBy = ["default.target"];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.aria}/bin/aria2c --console-log-level=warn --log-level=notice --enable-rpc --rpc-secret="${secrets.aria2_config.rpc_secret}" --rpc-listen-all --rpc-listen-port=6800 --dir="${config.xdg.userDirs.download}"
-      '';
+    aria2c = {
+      Unit = {
+        Description = "Aria2c Download Manager";
+        Documentation = ["man:aria2c(1)"];
+        After = ["network.target"];
+      };
+      Install = {
+        WantedBy = ["default.target" "ariang.service"];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.aria}/bin/aria2c --console-log-level=warn --log-level=notice --enable-rpc --rpc-secret="${secrets.aria2_config.rpc_secret}" --rpc-listen-port=6800 --rpc-allow-origin-all --dir="${config.xdg.userDirs.download}"
+        '';
+      };
     };
   };
 }
