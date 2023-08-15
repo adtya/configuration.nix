eaab371da3515a31978977e4630fcf5c4b585396
Adithya Nair <adtya@adtya.xyz>
2023-08-15 11:26:57 +0530

    handle external monitor in DP-3

--- home/wm/addon/kanshi.nix
+++ home/wm/addon/kanshi.nix
@@ -44,6 +44,22 @@ _: {
           }
         ];
       };
+      docked_3 = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            transform = "normal";
+            mode = "1920x1080";
+            position = "0,216";
+            scale = 1.25;
+          }
+          {
+            criteria = "DP-3";
+            transform = "normal";
+            position = "1536,0";
+          }
+        ];
+      };
     };
   };
 }
