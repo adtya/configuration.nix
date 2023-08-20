afefe46b332a7e441acbae2f7ca923151fc08d3e
Adithya Nair <adtya@adtya.xyz>
2023-08-20 19:42:57 +0530

    setup frpc for matrix

--- hosts/rico1/services/frpc.nix
+++ hosts/rico1/services/frpc.nix
@@ -29,5 +29,20 @@ in {
     type = tcp
     local_port = 22
     remote_port = 6001
+
+    [http.matrix.adtya.xyz]
+    type = http
+    custom_domains = matrix.adtya.xyz
+    local_port = 80
+
+    [https.matrix.adtya.xyz]
+    type = https
+    custom_domains = matrix.adtya.xyz
+    local_port = 443
+
+    [https.matrix.adtya.xyz.8448]
+    type = tcp
+    local_port = 8448
+    remote_port = 8448
   '';
 }
