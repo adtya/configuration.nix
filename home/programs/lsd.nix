30fa87189b4c733c7601af899ff793b8d004e498
Adithya Nair <adtya@adtya.xyz>
2023-09-09 21:35:35 +0530

    configure lsd

--- home/programs/lsd.nix
+++ home/programs/lsd.nix
@@ -1,8 +1,27 @@
-_: {
+{pkgs, ...}: let
+  dracula-lsd = pkgs.fetchFromGitHub {
+    owner = "dracula";
+    repo = "lsd";
+    rev = "75f3305a2bba4dacac82b143a15d278daee28232";
+    hash = "sha256-ZNyNYJEffxxU7K/7JOv+VRD3JbhW42kvmWk7pLu1vA8=";
+  };
+in {
+  xdg.configFile."lsd/themes/colors.yaml".source = "${dracula-lsd}/dracula.yaml";
   programs.lsd = {
     enable = true;
     enableAliases = true;
     settings = {
+      color = {
+        when = "always";
+        theme = "custom";
+      };
+      icons = {
+        when = "always";
+        theme = "fancy";
+      };
+      sorting = {
+        dir-grouping = "first";
+      };
       date = "relative";
     };
   };
