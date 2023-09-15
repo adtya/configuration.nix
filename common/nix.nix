1ab24c11834adb1b4dc822f049c6f3e40b14dcd6
Adithya Nair <adtya@adtya.xyz>
2023-09-16 01:45:59 +0530

    enable sandbox

--- common/nix.nix
+++ common/nix.nix
@@ -12,13 +12,14 @@
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes" "auto-allocate-uids" "cgroups" "ca-derivations"];
       auto-allocate-uids = true;
-      use-cgroups = true;
+      sandbox = true;
       trusted-substituters = [
         "https://nix-community.cachix.org/"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      use-cgroups = true;
     };
   };
 }
