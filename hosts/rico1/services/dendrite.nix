01e9ae7271ec4050d617003e1a43359b5c74f5fa
Adithya Nair <adtya@adtya.xyz>
2023-10-02 17:17:24 +0530

    setup dendrite database

--- hosts/rico1/services/dendrite.nix
+++ hosts/rico1/services/dendrite.nix
@@ -5,15 +5,45 @@ _: {
         reverse_proxy /_matrix/* 127.0.0.1:8008
       '';
     };
-    dendrite = {
+    dendrite = let
+      database = {
+        connection_string = "postgresql://dendrite@localhost/dendrite?sslmode=disable";
+      };
+    in {
       enable = true;
       settings = {
         global = {
           server_name = "adtya.xyz";
           private_key = "/etc/dendrite/matrix_key.pem";
-          database = {
-            connection_string = "postgresql://dendrite@localhost/dendrite?sslmode=disable";
-          };
+          inherit database;
+        };
+        app_service_api = {
+          inherit database;
+        };
+        federation_api = {
+          inherit database;
+        };
+        key_server = {
+          inherit database;
+        };
+        media_api = {
+          inherit database;
+        };
+        mscs = {
+          inherit database;
+        };
+        relay_api = {
+          inherit database;
+        };
+        room_server = {
+          inherit database;
+        };
+        sync_api = {
+          inherit database;
+        };
+        user_api = {
+          account_database = database;
+          device_database = database;
         };
       };
     };
