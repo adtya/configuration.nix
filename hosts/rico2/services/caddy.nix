69ee079b2e7bb789dcabbc9928280e73d59580c3
Adithya Nair <adtya@adtya.xyz>
2023-10-28 04:16:00 +0530

    use system

--- hosts/rico2/services/caddy.nix
+++ hosts/rico2/services/caddy.nix
@@ -17,7 +17,7 @@
 
       "if3.adtya.xyz" = {
         extraConfig = ''
-          root * ${if3.packages.default}/share/web
+          root * ${if3.packages.${config.nixpkgs.system}.default}/share/web
           encode gzip
           try_files {path} /index.html
           file_server
