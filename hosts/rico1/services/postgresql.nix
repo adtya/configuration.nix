9d02b6600e33309c5e9d3f112f6c341cf95710b2
Adithya Nair <adtya@adtya.xyz>
2023-10-29 23:53:58 +0530

    update postgres auth config

--- hosts/rico1/services/postgresql.nix
+++ hosts/rico1/services/postgresql.nix
@@ -1,6 +1,9 @@
 _: {
   services.postgresql = {
     enable = true;
+    authentication = ''
+      host  dendrite dendrite  127.0.0.1/32  trust
+    '';
     ensureDatabases = ["dendrite"];
     ensureUsers = [
       {
