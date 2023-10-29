830f5a5ef389def39c0b0a2641d35d87e4c2d5d4
Adithya Nair <adtya@adtya.xyz>
2023-10-29 23:38:56 +0530

    update dendrite config

--- hosts/rico1/services/dendrite/default.nix
+++ hosts/rico1/services/dendrite/default.nix
@@ -12,10 +12,17 @@
     wantedBy = ["multi-user.target"];
     serviceConfig = {
       Type = "simple";
+      DynamicUser = true;
+      StateDirectory = "dendrite";
+      WorkingDirectory = "/var/lib/dendrite";
+      RuntimeDirectory = "dendrite";
+      RuntimeDirectoryMode = "0700";
       LimitNOFILE = 65535;
       ExecStart = ''
         ${pkgs.dendrite}/bin/dendrite -http-bind-address 127.0.0.1:8008 -https-bind-address 127.0.0.1:8448 -config ${./config.yaml}
       '';
+      ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+      Restart = "on-failure";
     };
   };
 }
