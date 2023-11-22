91e02014a08c68057caaa002d3fa248ffac2b8c9
Adithya Nair <adtya@adtya.xyz>
2023-11-22 12:23:52 +0530

    reconfigure dns

--- hosts/skipper/network.nix
+++ hosts/skipper/network.nix
@@ -3,6 +3,13 @@
   networking = {
     hostName = "Skipper";
 
+    extraHosts = ''
+      10.8.1.1 proxy
+      10.8.1.2 skipper
+      10.8.1.3 rico1
+      10.8.1.4 rico2
+    '';
+
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
@@ -28,5 +35,14 @@
     };
   };
 
-  services.resolved.enable = true;
+  services.resolved = {
+    enable = true;
+    dnssec = "allow-downgrade";
+    extraConfig = ''
+      [Resolve]
+      DNS=9.9.9.11#dns11.quad9.net 2620:fe::11#dns11.quad9.net 149.112.112.11#dns11.quad9.net 2620:fe::fe:11#dns11.quad9.net
+      DNSOverTLS=yes
+      Domains=~.
+    '';
+  };
 }
