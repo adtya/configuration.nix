54a760e1d8e793747ed9a57ee3e99eee1b7c9c3d
Adithya Nair <adtya@adtya.xyz>
2023-11-25 17:00:58 +0530

    configure xdg portal

--- hosts/skipper/default.nix
+++ hosts/skipper/default.nix
@@ -65,6 +65,15 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk xdg-desktop-portal-hyprland ];
+    config = {
+      common = {
+        default = [ "gtk" ];
+      };
+      Hyprland = {
+        default = [ "hyprland" "gtk" ];
+      };
+    };
+    xdgOpenUsePortal = true;
   };
 
   system.stateVersion = "23.11";
