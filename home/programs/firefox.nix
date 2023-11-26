3a9e01c7d093d91236eb6ed0bd45027a420f3634
Adithya Nair <adtya@adtya.xyz>
2023-11-26 21:44:03 +0530

    add NixOS options to search engines

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -103,14 +103,24 @@
                 {
                   template = "https://search.nixos.org/packages";
                   params = [
-                    { name = "type"; value = "packages"; }
                     { name = "query"; value = "{searchTerms}"; }
                   ];
                 }
               ];
-
               icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
               definedAliases = [ "@np" ];
+            };
+            "NixOS Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@no" ];
             };
           };
           force = true;
