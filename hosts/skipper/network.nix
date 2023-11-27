d767531fa034c0f5f735fc0a17522428826a44d7
Adithya Nair <adtya@adtya.xyz>
2023-11-27 23:11:13 +0530

    update dns

--- hosts/skipper/network.nix
+++ hosts/skipper/network.nix
@@ -39,7 +39,8 @@
     enable = true;
     extraConfig = ''
       [Resolve]
-      DNS=9.9.9.11#dns11.quad9.net 2620:fe::11#dns11.quad9.net 149.112.112.11#dns11.quad9.net 2620:fe::fe:11#dns11.quad9.net
+      DNS=2620:fe::fe#dns.quad9.net 9.9.9.9#dns.quad9.net 2620:fe::9#dns.quad9.net 149.112.112.112#dns.quad9.net
+      FallbackDNS=
       DNSOverTLS=opportunistic
       Domains=~.
     '';
