8cd6d3f7710b14c4aab7fd48bb52dc1a08839e04
Adithya Nair <adtya@adtya.xyz>
2024-01-04 21:51:15 +0530

    setup dns on rico* hosts

--- hosts/rico2/network.nix
+++ hosts/rico2/network.nix
@@ -28,5 +28,14 @@
     };
   };
 
-  services.resolved.enable = true;
+  services.resolved = {
+    enable = true;
+    extraConfig = ''
+      [Resolve]
+      DNS=2620:fe::fe#dns.quad9.net 9.9.9.9#dns.quad9.net 2620:fe::9#dns.quad9.net 149.112.112.112#dns.quad9.net
+      FallbackDNS=
+      DNSOverTLS=opportunistic
+      Domains=~.
+    '';
+  };
 }
