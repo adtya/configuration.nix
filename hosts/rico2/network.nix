ee74395683b86a845d920f5e407a6fcf1cd59033
Adithya Nair <adtya@adtya.xyz>
2024-01-04 21:51:16 +0530

    clean up

--- hosts/rico2/network.nix
+++ hosts/rico2/network.nix
@@ -31,7 +31,6 @@
   services.resolved = {
     enable = true;
     extraConfig = ''
-      [Resolve]
       DNS=2620:fe::fe#dns.quad9.net 9.9.9.9#dns.quad9.net 2620:fe::9#dns.quad9.net 149.112.112.112#dns.quad9.net
       FallbackDNS=
       DNSOverTLS=opportunistic
