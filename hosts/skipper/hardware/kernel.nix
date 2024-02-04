dd3c0e36b9766a6367c00c72b471d5f779f0155f
Adithya Nair <adtya@adtya.xyz>
2024-02-04 17:02:05 +0530

    switch to xanmod kernel

--- hosts/skipper/hardware/kernel.nix
+++ hosts/skipper/hardware/kernel.nix
@@ -18,7 +18,7 @@
       systemd.enable = true;
     };
     kernelModules = [ "kvm-intel" ];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelParams = [ "quiet" ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
