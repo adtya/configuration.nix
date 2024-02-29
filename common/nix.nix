3ddb12d5df3e85633972b0a41e0c902d6600915a
Adithya Nair <adtya@adtya.xyz>
2024-02-29 15:29:28 +0530

    trust the wheel

--- common/nix.nix
+++ common/nix.nix
@@ -18,6 +18,7 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      trusted-users = [ "@wheel" ];
       use-cgroups = true;
     };
   };
