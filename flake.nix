5f7cdb99c75c03ad6a1601e5ea12dcce904a6105
Adithya Nair <adtya@adtya.xyz>
2024-03-22 23:59:14 +0530

    add binary caches

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "NixOS Configuration";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org/"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
