d446972eb66596142b4897005883cf355c8460f5
Adithya Nair <adtya@adtya.xyz>
2024-03-31 13:42:47 +0530

    log error only

--- home/services/transmission.nix
+++ home/services/transmission.nix
@@ -16,7 +16,7 @@
         Service = {
           Type = "notify";
           ExecStart = ''
-            ${transmission-daemon} -f --log-level=info --encryption-preferred --portmap --dht --lpd --utp --peerport 41414 --port 9092 \
+            ${transmission-daemon} -f --log-level=error --encryption-preferred --portmap --dht --lpd --utp --peerport 41414 --port 9092 \
               -c "${torrents-dir}/init" \
               --incomplete-dir "${torrents-dir}/.incomplete" \
               --download-dir "${torrents-dir}/downloads" \
