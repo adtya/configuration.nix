70ad9cdc589065491edb8696c26580487b92a6ba
Adithya Nair <adtya@adtya.xyz>
2024-04-12 22:53:30 +0530

    cleanup locales

--- hosts/skipper/default.nix
+++ hosts/skipper/default.nix
@@ -35,20 +35,20 @@
   gtk.iconCache.enable = true;
 
   i18n = {
-    defaultLocale = "en_US.UTF-8";
+    defaultLocale = "en_IN.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-      LC_ALL = "en_US.UTF-8";
+      LC_ADDRESS = "en_IN.UTF-8";
+      LC_IDENTIFICATION = "en_IN.UTF-8";
+      LC_MEASUREMENT = "en_IN.UTF-8";
+      LC_MONETARY = "en_IN.UTF-8";
+      LC_NAME = "en_IN.UTF-8";
+      LC_NUMERIC = "en_IN.UTF-8";
+      LC_PAPER = "en_IN.UTF-8";
+      LC_TELEPHONE = "en_IN.UTF-8";
+      LC_TIME = "en_IN.UTF-8";
+      LC_ALL = "en_IN.UTF-8";
     };
-    supportedLocales = [ "en_US.UTF-8/UTF-8" ];
+    supportedLocales = [ "en_IN/UTF-8" "en_US.UTF-8/UTF-8" "ml_IN/UTF-8" ];
   };
 
   services.xserver.xkb = {
