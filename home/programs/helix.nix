c977039e5263a2157b3730d0372cf78ce1bf5208
Adithya Nair <adtya@adtya.xyz>
2024-04-14 14:03:52 +0530

    configure helix

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -3,6 +3,15 @@ _: {
     enable = true;
     settings = {
       theme = "dracula";
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+        cursor-shape = {
+          insert = "bar";
+          select = "underline";
+          normal = "block";
+        };
+      };
     };
   };
 }
