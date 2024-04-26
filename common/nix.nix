856ddbff10b83419a3a974beb0bc36120e77a9d6
Adithya Nair <adtya@adtya.xyz>
2024-04-26 10:17:06 +0530

    setup substituters

--- common/nix.nix
+++ common/nix.nix
@@ -13,10 +13,16 @@
       auto-allocate-uids = true;
       sandbox = true;
       trusted-substituters = [
+        "https://cache.nixos.org/"
         "https://nix-community.cachix.org/"
+        "https://helix.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
       trusted-users = [ "@wheel" ];
       use-cgroups = true;
--- flake.nix
+++ flake.nix
@@ -3,14 +3,16 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org/"
       "https://helix.cachix.org"
       "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
