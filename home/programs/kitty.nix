b446072bbf2531d9a606a9057f206bb493e3dedd
Adithya Nair <adtya@adtya.xyz>
2024-04-27 23:08:01 +0530

    cleanup kitty config

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,34 +1,24 @@
-{ pkgs, ... }:
-let
-  dracula-kitty = pkgs.fetchFromGitHub {
-    owner = "dracula";
-    repo = "kitty";
-    rev = "87717a3f00e3dff0fc10c93f5ff535ea4092de70";
-    hash = "sha256-78PTH9wE6ktuxeIxrPp0ZgRI8ST+eZ3Ok2vW6BCIZkc=";
-  };
-in
-{
+{ pkgs, ... }: {
   programs.kitty = {
     enable = true;
-    font.package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
-    font.name = "FiraCode Nerd Font";
-    extraConfig = ''
-      font_size	14
-      initial_window_width	100c
-      initial_window_height	25c
-      window_padding_width	4.0
-      background_opacity	0.98
-      cursor_shape	beam
-      scrollback_lines	2000
-      copy_on_select	clipboard
-      url_style	curly
-      sync_to_monitor	yes
-      shell_integration disabled
-
-      include ${dracula-kitty}/dracula.conf
-    '';
+    font = {
+      package = pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; };
+      name = "FiraCode Nerd Font";
+      size = 14;
+    };
     settings = {
       enable_audio_bell = false;
+      initial_window_width = "100c";
+      initial_window_height = "25c";
+      window_padding_width = "4.0";
+      background_opacity = "0.98";
+      cursor_shape = "beam";
+      scrollback_lines = 2000;
+      copy_on_select = "clipboard";
+      url_style = "curly";
+      sync_to_monitor = true;
     };
+    shellIntegration.mode = "disabled";
+    theme = "Dracula";
   };
 }
