c82061e7526266b40e730eef1d9db3fe4d7e86bf
Adithya Nair <adtya@adtya.xyz>
2024-05-12 19:05:49 +0530

    configure hyprlock

--- home/wm/addon/hyprlock.nix
+++ home/wm/addon/hyprlock.nix
@@ -1,5 +1,15 @@
 _: {
   programs.hyprlock = {
     enable = true;
+    settings = {
+      general = {
+        ignore_empty_input = true;
+      };
+      background = {
+        path = "screenshot";
+        blur_passes = 2;
+        blur_size = 5;
+      };
+    };
   };
 }
