a403f9ddbfc8c33b2a075528bb1ec42cf6492a3c
Adithya Nair <adtya@adtya.xyz>
2024-05-12 13:27:37 +0530

    cleanup kanshi config

--- home/wm/addon/kanshi.nix
+++ home/wm/addon/kanshi.nix
@@ -2,64 +2,77 @@ _: {
   services.kanshi = {
     enable = true;
     systemdTarget = "graphical-session.target";
-    profiles = {
-      undocked = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            mode = "1920x1080";
-            position = "0,0";
-          }
-        ];
-      };
-      docked_1 = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            transform = "normal";
-            mode = "1920x1080";
-            position = "0,216";
-            scale = 1.25;
-          }
-          {
-            criteria = "DP-1";
-            transform = "normal";
-            position = "1536,0";
-          }
-        ];
-      };
-      docked_2 = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            transform = "normal";
-            mode = "1920x1080";
-            position = "0,216";
-            scale = 1.25;
-          }
-          {
-            criteria = "DP-2";
-            transform = "normal";
-            position = "1536,0";
-          }
-        ];
-      };
-      docked_3 = {
-        outputs = [
-          {
-            criteria = "eDP-1";
-            transform = "normal";
-            mode = "1920x1080";
-            position = "0,216";
-            scale = 1.25;
-          }
-          {
-            criteria = "DP-3";
-            transform = "normal";
-            position = "1536,0";
-          }
-        ];
-      };
-    };
+    settings = [
+      {
+        profile = {
+          name = "undocked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              mode = "1920x1080";
+              position = "0,0";
+            }
+
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked_1";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              transform = "normal";
+              mode = "1920x1080";
+              position = "0,216";
+              scale = 1.25;
+            }
+            {
+              criteria = "DP-1";
+              transform = "normal";
+              position = "1536,0";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked_2";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              transform = "normal";
+              mode = "1920x1080";
+              position = "0,216";
+              scale = 1.25;
+            }
+            {
+              criteria = "DP-2";
+              transform = "normal";
+              position = "1536,0";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked_3";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              transform = "normal";
+              mode = "1920x1080";
+              position = "0,216";
+              scale = 1.25;
+            }
+            {
+              criteria = "DP-3";
+              transform = "normal";
+              position = "1536,0";
+            }
+          ];
+        };
+      }
+    ];
   };
 }
