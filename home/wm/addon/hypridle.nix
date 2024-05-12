3cdba5762a227f92ec5cc8abafcc6499f13ee2f4
Adithya Nair <adtya@adtya.xyz>
2024-05-12 21:42:10 +0530

    configure hypridle and remove swayidle

--- home/wm/addon/hypridle.nix
+++ home/wm/addon/hypridle.nix
@@ -1,8 +1,44 @@
-{pkgs, ...}: {
+{ config, pkgs, ... }:
+let
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
+  lock-session = "${pkgs.systemd}/bin/loginctl lock-session";
+in
+{
   services.hypridle = {
     enable = true;
     settings = {
-      lock_cmd = "${pkgs.hyprlock}/bin/hyprlock";
+      general = {
+        lock_cmd = "${pkgs.hyprlock}/bin/hyprlock";
+        unlock_cmd = "${pkgs.procps}/bin/pkill -USR1 hyprlock";
+        before_sleep_cmd = lock-session;
+        after_sleep_cmd = "${hyprctl} dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 240;
+          on-timeout = "${brightnessctl} -s set 30";
+          on-resume = "${brightnessctl} -r";
+        }
+        {
+          timeout = 240;
+          on-timeout = "${brightnessctl} -sd dell::kbd_backlight set 0";
+          on-resume = "${brightnessctl} -rd dell::kbd_backlight";
+        }
+        {
+          timeout = 600;
+          on-timeout = lock-session;
+        }
+        {
+          timeout = 660;
+          on-timeout = "${hyprctl} dispatch dpms off";
+          on-resume = "${hyprctl} dispatch dpms on";
+        }
+        {
+          timeout = 900;
+          on-timeout = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
     };
   };
 }
--- home/wm/hyprland/default.nix
+++ home/wm/hyprland/default.nix
@@ -10,7 +10,7 @@ let
   kitty = "${config.programs.kitty.package}/bin/kitty";
   playerctl = "${pkgs.playerctl}/bin/playerctl";
   rofi = "${config.programs.rofi.package}/bin/rofi";
-  hyprlock = "${config.programs.hyprlock.package}/bin/hyprlock";
+  loginctl = "${pkgs.systemd}/bin/loginctl";
   tmux = "${config.programs.tmux.package}/bin/tmux";
   wpctl = "${pkgs.wireplumber}/bin/wpctl";
 
@@ -165,7 +165,7 @@ in
         "SUPER_SHIFT,y,       exec, ${pkgs.scripts}/bin/youtube"
         "SUPER_SHIFT,b,       exec, ${pkgs.rofi-bluetooth}/bin/rofi-bluetooth"
 
-        "SUPER,escape,        exec, ${hyprlock}"
+        "SUPER,escape,        exec, ${loginctl} lock-session"
         "SUPER_SHIFT,W,       exec, systemctl --user start wallpaper.service"
 
         "SUPER,1,             workspace, 1"
