ce131b61180be174f241e32b2273fba8045c4233
Adithya Nair <adtya@adtya.xyz>
2024-05-13 00:33:44 +0530

    configure yazi

--- home/programs/files.nix
+++ home/programs/files.nix
@@ -12,6 +12,17 @@ _: {
     yazi = {
       enable = true;
       enableZshIntegration = true;
+      settings = {
+        manager = {
+          sort_by = "natural";
+          sort_dir_first = true;
+          show_hidden = true;
+
+        };
+        preview = {
+          image_filter = "lanczos3";
+        };
+      };
     };
   };
 
