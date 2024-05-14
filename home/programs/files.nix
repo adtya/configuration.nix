1fcbc951b73caf56d0ded5626d9e996bd1c50812
Adithya Nair <adtya@adtya.xyz>
2024-05-14 23:15:50 +0530

    setup openers in yazi

--- home/programs/files.nix
+++ home/programs/files.nix
@@ -1,4 +1,4 @@
-_: {
+{ config, ... }: {
   programs = {
     fd = {
       enable = true;
@@ -16,7 +16,28 @@ _: {
         };
         preview = {
           image_filter = "lanczos3";
+          image_quality = 90;
         };
+        opener = {
+          open-image = [
+            { run = "${config.programs.imv.package}/bin/imv $0"; orphan = true; }
+          ];
+          edit-text = [
+            { run = "${config.programs.neovim.package}/bin/nvim $0"; block = true; }
+          ];
+          play = [
+            { run = "${config.programs.mpv.package}/bin/mpv $0"; orphan = true; }
+          ];
+          terminal = [
+            { run = "${config.programs.kitty.package}/bin/kitty --class=yazi -d=$0"; orphan = true; }
+          ];
+        };
+        open.rules = [
+          { mime = "image/*"; use = "open-image"; }
+          { mime = "text/*"; use = [ "edit-text" ]; }
+          { mime = "video/*"; use = [ "play" ]; }
+          { mime = "inode/directory"; use = [ "terminal" ]; }
+        ];
       };
     };
   };
