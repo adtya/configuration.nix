c01de78c59f8104b02e7b72c291ba5d0f226e9cc
Adithya Nair <adtya@adtya.xyz>
2024-05-15 02:24:54 +0530

    show password input field

--- home/wm/addon/hyprlock.nix
+++ home/wm/addon/hyprlock.nix
@@ -4,11 +4,27 @@ _: {
     settings = {
       general = {
         ignore_empty_input = true;
+        hide_cursor = true;
       };
       background = {
         path = "screenshot";
         blur_passes = 2;
         blur_size = 5;
+      };
+      input-field = {
+        outer_color = "rgb(bd93f9)";
+        inner_color = "rgb(282a36)";
+        font_color = "rgb(f8f8f2)";
+        check_color = "rgb(f1fa8c)";
+        fail_color = "rgb(ff5555)";
+        size = "300, 48";
+        outline_thickness = 1;
+        position = "0, 48";
+        rounding = 10;
+        halign = "center";
+        valign = "bottom";
+        placeholder_text = ''<span font_family="Fira Code">password</span>'';
+        fail_text = "🖕";
       };
     };
   };
