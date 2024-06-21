8344439457ff3283915df76699f507fd9b89b572
Adithya Nair <adtya@adtya.xyz>
2024-06-21 20:20:10 +0530

    switch wireguard endpoint to ip

--- hosts/skipper/wireguard.nix
+++ hosts/skipper/wireguard.nix
@@ -14,11 +14,10 @@
         peers = [
           {
             name = "Proxy";
-            endpoint = "proxy.adtya.xyz:51821";
+            endpoint = "165.232.180.97:51821";
             publicKey = "NNw/iDMCTq8mpHncrecEh4UlvtINX/UUDtCJf2ToFR4=";
             presharedKeyFile = config.sops.secrets."wireguard/psk".path;
             persistentKeepalive = 20;
-            dynamicEndpointRefreshSeconds = 20;
             allowedIPs = [
               "10.10.10.0/24"
               "fd7c:585c:c4ae::0/64"
