63ba76dc75e920be981be30a0e67fcca6df4b33f
Adithya Nair <adtya@adtya.xyz>
2024-06-21 22:02:51 +0530

    fix rootless docker dns

--- hosts/skipper/virtualisation.nix
+++ hosts/skipper/virtualisation.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   environment.systemPackages = [ pkgs.docker-credential-helpers ];
   virtualisation = {
     docker = {
@@ -6,13 +6,21 @@
       package = pkgs.docker_26;
       daemon.settings = {
         data-root = "/persist/docker";
+        dns = [
+          # Quad9, without DoT
+          "2620:fe::fe"
+          "9.9.9.9"
+          "2620:fe::9"
+          "149.112.112.112"
+        ];
       };
       rootless = {
         enable = true;
-        package = pkgs.docker_26;
+        package = config.virtualisation.docker.package;
         setSocketVariable = true;
         daemon.settings = {
           data-root = "/persist/home/docker";
+          dns = config.virtualisation.docker.daemon.settings.dns;
         };
       };
       storageDriver = "btrfs";
