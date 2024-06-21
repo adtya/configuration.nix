15e6749bd66db8f9dab4c803d35fd5b1b453c168
Adithya Nair <adtya@adtya.xyz>
2024-06-21 19:48:25 +0530

    cleanup user groups

--- common/users.nix
+++ common/users.nix
@@ -9,7 +9,7 @@
       hashedPasswordFile = config.sops.secrets."passwd/adtya".path;
       description = "Adithya Nair";
       isNormalUser = true;
-      extraGroups = [ "docker" "libvirtd" "networkmanager" "tss" "wheel" ];
+      extraGroups = [ "docker" "libvirtd" "networkmanager" "wheel" ];
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxDgoV9yf+yPnp4pt5EWgo7uC25W66ehoL/rlshVW+8 Skipper"
