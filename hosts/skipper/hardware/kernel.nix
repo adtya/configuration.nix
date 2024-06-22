6d0468fcb924646147fe639f1a7e36bd0f3e7c04
Adithya Nair <adtya@adtya.xyz>
2024-06-22 21:48:37 +0530

    cleanup kernel modules

--- hosts/skipper/hardware/kernel.nix
+++ hosts/skipper/hardware/kernel.nix
@@ -8,10 +8,7 @@
       availableKernelModules = [
         "xhci_pci"
         "thunderbolt"
-        "vmd"
         "nvme"
-        "usb_storage"
-        "sd_mod"
         "rtsx_pci_sdmmc"
       ];
       kernelModules = [ "i915" "dm-snapshot" ];
