98d7b547ec2dedef378274ff4ff5c0ab5e82efc2
Adithya Nair <adtya@adtya.xyz>
2024-07-03 18:36:19 +0530

    wynne: use nomodeset

--- hosts/wynne/hardware/default.nix
+++ hosts/wynne/hardware/default.nix
@@ -10,12 +10,12 @@
     consoleLogLevel = 3;
     initrd = {
       availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "sd_mod" ];
-      kernelModules = [ "i915" ];
+      kernelModules = [ ];
       systemd.enable = true;
     };
     kernelModules = [ "kvm-intel" ];
     kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-    kernelParams = [ "quiet" ];
+    kernelParams = [ "quiet" "nomodeset" ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
       "vm.dirty_ratio" = 3;
