5a9442c55c2bd95984c9a132ec3b6524d3e43839
Adithya Nair <adtya@adtya.xyz>
2024-07-04 01:13:49 +0530

    rico0: setup transmission

---
+++ hosts/rico0/services/apps/transmission.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  services.transmission = {
+    enable = true;
+    package = pkgs.transmission_4;
+    downloadDirPermissions = "775";
+    home = "/mnt/data/Torrents";
+    webHome = pkgs.flood-for-transmission;
+    openPeerPorts = true;
+    extraFlags = [
+      "--encryption-required"
+      "--no-portmap"
+      "--dht"
+      "--lpd"
+      "--allowed" "127.0.0.1,10.10.10.*"
+    ];
+    settings = {
+      peer-port = 51515;
+      rpc-bind-address = "10.10.10.10";
+      rpc-port = 9091;
+      watch-dir-enabled = true;
+    };
+  };
+  systemd.services.transmission.after = [ "mnt-data.mount" ];
+}
--- hosts/rico0/services/default.nix
+++ hosts/rico0/services/default.nix
@@ -2,6 +2,8 @@ _: {
   imports = [
     ./btrfs.nix
     ./ssh.nix
+
+    ./apps
   ];
 
 }
