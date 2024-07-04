2b5164aa49ee33da25ee7ab6608f83d3efa4e1f0
Adithya Nair <adtya@adtya.xyz>
2024-07-05 00:31:34 +0530

    wynne: minimize

--- hosts/wynne/programs/default.nix
+++ hosts/wynne/programs/default.nix
@@ -1,17 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   imports = [
     ./neovim.nix
     ./starship.nix
     ./zsh.nix
   ];
 
-  programs.git.enable = true;
+  programs = {
+    command-not-found.enable = lib.mkDefault false;
+    git.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     sops
     age
-    libraspberrypi
-    raspberrypi-eeprom
   ];
-
 }
