16143e92bb047860a38662302c7ca8be82532a28
Adithya Nair <adtya@adtya.xyz>
2024-07-17 11:18:33 +0530

    rico0: https for prometheus

--- hosts/rico0/services/apps/prometheus.nix
+++ hosts/rico0/services/apps/prometheus.nix
@@ -1,20 +1,31 @@
 _: {
-  services.prometheus = {
-    enable = true;
-    listenAddress = "10.10.10.10";
-    scrapeConfigs = [
-      {
-        job_name = "frp";
-        static_configs = [
-          { targets = [ "10.10.10.1:7500" ]; }
-        ];
-      }
-      {
-        job_name = "blocky";
-        static_configs = [
-          { targets = [ "10.10.10.10:8080" ]; }
-        ];
-      }
-    ];
+  services = {
+    caddy = {
+      virtualHosts."prometheus.labs.adtya.xyz" = {
+        extraConfig = ''
+          reverse_proxy 127.0.0.1:9090
+          tls /persist/secrets/caddy/certs/prometheus.crt /persist/secrets/caddy/certs/prometheus.key
+        '';
+      };
+    };
+    prometheus = {
+      enable = true;
+      listenAddress = "127.0.0.1";
+      scrapeConfigs = [
+        {
+          job_name = "frp";
+          scheme = "https";
+          static_configs = [
+            { targets = [ "frp.labs.adtya.xyz" ]; }
+          ];
+        }
+        {
+          job_name = "blocky";
+          static_configs = [
+            { targets = [ "10.10.10.10:8080" ]; }
+          ];
+        }
+      ];
+    };
   };
 }
