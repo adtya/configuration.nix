643ae6cce39e37a787b9ee15c39f956ef2097edc
Adithya Nair <adtya@adtya.xyz>
2024-07-17 11:05:14 +0530

    layne: configure rpc-host-whitelist for transmission

--- hosts/layne/services/apps/transmission.nix
+++ hosts/layne/services/apps/transmission.nix
@@ -3,7 +3,7 @@
     caddy = {
       virtualHosts."transmission.labs.adtya.xyz" = {
         extraConfig = ''
-          reverse_proxy 10.10.10.14:9091
+          reverse_proxy 127.0.0.1:9091
           tls /persist/secrets/caddy/certs/transmission.crt /persist/secrets/caddy/certs/transmission.key
         '';
       };
@@ -20,13 +20,12 @@
         "--no-portmap"
         "--dht"
         "--lpd"
-        "--allowed"
-        "127.0.0.1,10.10.10.*"
       ];
       settings = {
         peer-port = 51515;
-        rpc-bind-address = "10.10.10.14";
+        rpc-bind-address = "127.0.0.1";
         rpc-port = 9091;
+        rpc-host-whitelist = "transmission.labs.adtya.xyz";
         watch-dir-enabled = true;
       };
     };
