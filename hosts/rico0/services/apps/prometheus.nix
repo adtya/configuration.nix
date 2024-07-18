b8f24c51b22c1e12e9b8119bac01965048c87e73
Adithya Nair <adtya@adtya.xyz>
2024-07-18 22:29:43 +0530

    rico0: add node-exporter metrics in prometheus

--- hosts/rico0/services/apps/prometheus.nix
+++ hosts/rico0/services/apps/prometheus.nix
@@ -13,6 +13,17 @@ _: {
       listenAddress = "127.0.0.1";
       scrapeConfigs = [
         {
+          job_name = "node";
+          scheme = "https";
+          static_configs = [
+            { targets = [ "rico0.labs.adtya.xyz" ]; }
+            { targets = [ "rico1.labs.adtya.xyz" ]; }
+            { targets = [ "rico2.labs.adtya.xyz" ]; }
+            { targets = [ "wynne.labs.adtya.xyz" ]; }
+            { targets = [ "layne.labs.adtya.xyz" ]; }
+          ];
+        }
+        {
           job_name = "frp";
           scheme = "https";
           static_configs = [
