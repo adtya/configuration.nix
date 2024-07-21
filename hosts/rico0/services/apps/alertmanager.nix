4a24b9a502b58c89d1f7996e84374fca8df98687
Adithya Nair <adtya@adtya.xyz>
2024-07-21 19:27:58 +0530

    rico0: configure alertmanager

--- hosts/rico0/services/apps/alertmanager.nix
+++ hosts/rico0/services/apps/alertmanager.nix
@@ -13,6 +13,32 @@ _: {
       listenAddress = "127.0.0.1";
       port = 9093;
       webExternalUrl = "https://alertmanager.labs.adtya.xyz/";
+      configuration = {
+        route = {
+          group_by = [ "alertname" ];
+          group_wait = "30s";
+          group_interval = "5m";
+          repeat_interval = "1h";
+          receiver = "web.hook";
+        };
+        receivers = [
+          {
+            name = "web.hook";
+            webhook_configs = [
+              { url = "http://127.0.0.1:5001/"; }
+            ];
+          }
+        ];
+        inhibit_rules = [
+          {
+            source_match =
+              { severity = "critical"; };
+            target_match =
+              { severity = "warning"; };
+            equal = [ "alertname" "dev" "instance" ];
+          }
+        ];
+      };
     };
   };
 }
