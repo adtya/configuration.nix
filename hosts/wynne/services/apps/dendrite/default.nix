8a55dd4afbaa10a4f18774b2f29e11cdc4e1e9de
Adithya Nair <adtya@adtya.xyz>
2024-09-09 19:22:31 +0530

    wynne: setup sliding-sync for dedrite

--- hosts/wynne/services/apps/dendrite/default.nix
+++ hosts/wynne/services/apps/dendrite/default.nix
@@ -1,8 +1,20 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
+  sops = {
+    secrets = {
+      "matrix/syncv3_secret" = {
+        mode = "444";
+        owner = config.users.users.root.name;
+        group = config.users.users.root.group;
+      };
+    };
+  };
   services = {
     caddy.virtualHosts."matrix.acomputer.lol" = {
       extraConfig = ''
+        reverse_proxy /client/* 127.0.0.1:8009
+        reverse_proxy /_matrix/client/unstable/org.matrix.msc3575/sync 127.0.0.1:8009
         reverse_proxy /_matrix/* 127.0.0.1:8008
+        reverse_proxy /_dendrite/* 127.0.0.1:8008
         reverse_proxy /_synapse/* 127.0.0.1:8008
       '';
     };
@@ -22,6 +34,15 @@
         transport.useCompression = true;
       }
     ];
+    matrix-sliding-sync = {
+      enable = true;
+      settings = {
+        SYNCV3_SERVER = "https://matrix.acomputer.lol";
+        SYNCV3_BINDADDR = "127.0.0.1:8009";
+        SYNCV3_DB = "postgresql://dendrite@localhost/dendrite?sslmode=disable";
+      };
+      environmentFile = config.sops.secrets."matrix/syncv3_secret".path;
+    };
   };
   systemd.services.dendrite = {
     description = "Dendrite Matrix homeserver";
