2413dd79352729f501f91801f1aba89a1c4472db
Adithya Nair <adtya@adtya.xyz>
2024-09-14 16:08:26 +0530

    all: use default CA from caddy

--- hosts/shared/caddy.nix
+++ hosts/shared/caddy.nix
@@ -1,7 +1,6 @@
 { inputs, pkgs, ... }: {
   services.caddy = {
     enable = true;
-    acmeCA = "https://acme-v02.api.letsencrypt.org/directory";
     package = inputs.caddy.packages.${pkgs.system}.caddy;
     email = "admin@acomputer.lol";
   };
