1b054616a31cbd933718abda2c6613ab748552dc
Adithya Nair <adtya@adtya.xyz>
2024-09-14 17:42:30 +0530

    rico0: remove remaining custom tls certs

--- hosts/rico0/services/default.nix
+++ hosts/rico0/services/default.nix
@@ -12,31 +12,26 @@ _: {
       "gateway.labs.adtya.xyz" = {
         extraConfig = ''
           reverse_proxy 192.168.0.1:80
-          tls /persist/secrets/caddy/certs/default.crt /persist/secrets/caddy/certs/default.key
         '';
       };
       "ap1.labs.adtya.xyz" = {
         extraConfig = ''
           reverse_proxy 192.168.1.1:80
-          tls /persist/secrets/caddy/certs/default.crt /persist/secrets/caddy/certs/default.key
         '';
       };
       "ap2.labs.adtya.xyz" = {
         extraConfig = ''
           reverse_proxy 192.168.1.2:80
-          tls /persist/secrets/caddy/certs/default.crt /persist/secrets/caddy/certs/default.key
         '';
       };
       "switch.labs.adtya.xyz" = {
         extraConfig = ''
           reverse_proxy 192.168.1.3:80
-          tls /persist/secrets/caddy/certs/default.crt /persist/secrets/caddy/certs/default.key
         '';
       };
       "frp.labs.adtya.xyz" = {
         extraConfig = ''
           reverse_proxy 10.10.10.1:7500
-          tls /persist/secrets/caddy/certs/default.crt /persist/secrets/caddy/certs/default.key
         '';
       };
     };
