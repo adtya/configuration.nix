36b1698953e78b7e258472ac5ff08cb4c7d9b1fd
Adithya Nair <adtya@adtya.xyz>
2024-09-14 16:04:50 +0530

    all: add personal cachix as trusted substituter

--- common/nix.nix
+++ common/nix.nix
@@ -9,10 +9,12 @@ _: {
       auto-allocate-uids = true;
       sandbox = true;
       trusted-substituters = [
+        "https://adtya.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
       ];
       trusted-public-keys = [
+        "adtya.cachix.org-1:lAuNLx0Ehzx6FoH20rVkMD7KyZZevlLfvm3lwMAzrnU="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
