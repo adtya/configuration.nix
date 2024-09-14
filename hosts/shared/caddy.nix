4b5463471a96de47bb17f4db8139f9c8807dc534
Adithya Nair <adtya@adtya.xyz>
2024-09-14 16:57:22 +0530

    all: setup caddy to use DNS challenge

--- hosts/shared/caddy.nix
+++ hosts/shared/caddy.nix
@@ -1,9 +1,22 @@
-{ inputs, pkgs, ... }: {
+{ config, inputs, pkgs, ... }: {
+  sops = {
+    secrets = {
+      "digitalocean/token_file" = {
+        mode = "444";
+        owner = config.users.users.root.name;
+        group = config.users.users.root.group;
+      };
+    };
+  };
   services.caddy = {
     enable = true;
     package = inputs.caddy.packages.${pkgs.system}.caddy;
     email = "admin@acomputer.lol";
+    globalConfig = ''
+      acme_dns digitalocean {env.DO_API_TOKEN}
+    '';
   };
+  systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.secrets."digitalocean/token_file".path;
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
 
