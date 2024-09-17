731e54c9c1be6a0f806820ff61af7e81728737ab
Adithya Nair <adtya@adtya.xyz>
2024-09-17 11:09:41 +0530

    all: promtail - label everything? :shrug:

--- hosts/shared/promtail.nix
+++ hosts/shared/promtail.nix
@@ -21,12 +21,60 @@
               json = false;
               max_age = "12h";
               path = "/var/log/journal";
-              labels = { job = "systemd-journal"; host = "${config.networking.hostName}"; };
+              labels = { job = "systemd-journal"; };
             };
             relabel_configs = [
               {
+                source_labels = [ "__journal__systemd_cgroup" ];
+                target_label = "systemd_cgroup";
+              }
+              {
+                source_labels = [ "__journal__systemd_slice" ];
+                target_label = "systemd_slice";
+              }
+              {
                 source_labels = [ "__journal__systemd_unit" ];
-                target_label = "unit";
+                target_label = "systemd_unit";
+              }
+              {
+                source_labels = [ "__journal__systemd_user_unit" ];
+                target_label = "systemd_user_unit";
+              }
+              {
+                source_labels = [ "__journal__systemd_user_slice" ];
+                target_label = "systemd_user_slice";
+              }
+              {
+                source_labels = [ "__journal__systemd_session" ];
+                target_label = "systemd_session";
+              }
+              {
+                source_labels = [ "__journal__systemd_owner_uid" ];
+                target_label = "systemd_owner_uid";
+              }
+              {
+                source_labels = [ "__journal__hostname" ];
+                target_label = "node_name";
+              }
+              {
+                source_labels = [ "__journal_syslog_identifier" ];
+                target_label = "syslog_identifier";
+              }
+              {
+                source_labels = [ "__journal__transport" ];
+                target_label = "transport";
+              }
+              {
+                source_labels = [ "__journal__pid" ];
+                target_label = "pid";
+              }
+              {
+                source_labels = [ "__journal__uid" ];
+                target_label = "uid";
+              }
+              {
+                source_labels = [ "__journal__gid" ];
+                target_label = "gid";
               }
             ];
           }
