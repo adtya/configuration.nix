9ca5d889b056a2a2e0e8ff7a93641aa5697b6d2c
Adithya Nair <adtya@adtya.xyz>
2024-09-28 22:39:47 +0530

    skipper: eww, the nix way

--- home/wm/addon/eww/default.nix
+++ home/wm/addon/eww/default.nix
@@ -1,6 +1,63 @@
-_: {
+{ lib, config, pkgs, ... }:
+let
+  configFile = pkgs.substituteAll {
+    src = ./config/eww.yuck;
+    test_string = "Testing Out Eww!";
+  };
+
+  cssFile = ./config/eww.scss;
+
+  configDir = _: pkgs.stdenvNoCC.mkDerivation {
+    name = "eww-config";
+
+    srcs = [ configFile cssFile ];
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out
+      for src in $srcs; do
+        cp "$src" $out/$(stripHash "$src")
+      done
+    '';
+  };
+in
+{
   programs.eww = {
     enable = true;
-    configDir = ./config;
+    configDir = pkgs.callPackage configDir { };
+  };
+  systemd.user.services = {
+    eww = {
+      Unit = {
+        Description = "ElKowars wacky widgets - daemon";
+        Documentation = "https://elkowar.github.io/eww/";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session-pre.target" ];
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${lib.getExe config.programs.eww.package} daemon --no-daemonize --logs";
+        Restart = "on-failure";
+        KillMode = "mixed";
+      };
+    };
+    eww-widgets = {
+      Unit = {
+        Description = "ElKowars wacky widgets";
+        Documentation = "https://elkowar.github.io/eww/";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session-pre.target" ];
+        Requires = [ "eww.service" ];
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "${lib.getExe config.programs.eww.package} open activate-notice";
+      };
+    };
   };
 }
