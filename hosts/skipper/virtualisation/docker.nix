c0a108859f77d191dc52ef86619198ab5e10dfbc
Adithya Nair <adtya@adtya.xyz>
2024-10-27 15:14:42 +0530

    skipper: remove rootless docker

--- hosts/skipper/virtualisation/docker.nix
+++ hosts/skipper/virtualisation/docker.nix
@@ -1,27 +1,10 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   environment.systemPackages = [ pkgs.docker-credential-helpers ];
   virtualisation = {
     docker = {
       enable = true;
-      package = pkgs.docker_26;
       daemon.settings = {
         data-root = "/persist/docker";
-        dns = [
-          # Quad9, without DoT
-          "2620:fe::fe"
-          "9.9.9.9"
-          "2620:fe::9"
-          "149.112.112.112"
-        ];
-      };
-      rootless = {
-        enable = true;
-        package = config.virtualisation.docker.package;
-        setSocketVariable = true;
-        daemon.settings = {
-          data-root = "/persist/home/docker";
-          dns = config.virtualisation.docker.daemon.settings.dns;
-        };
       };
       storageDriver = "btrfs";
     };
