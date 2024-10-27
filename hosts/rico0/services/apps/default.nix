00748475d1b5cd742d853cffa9dd3117e9acfe2c
Adithya Nair <adtya@adtya.xyz>
2024-10-27 19:06:04 +0530

    rico0: remove stuff already moved to rico1

--- hosts/rico0/services/apps/default.nix
+++ hosts/rico0/services/apps/default.nix
@@ -1,10 +1,5 @@
 _: {
   imports = [
-    ./alertmanager.nix
-    ./blocky.nix
-    ./grafana.nix
-    ./loki.nix
-    ./prometheus.nix
     ../../../shared/prometheus-exporters.nix
     ../../../shared/promtail.nix
   ];
