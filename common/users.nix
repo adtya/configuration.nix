f37c7c84203daeefdbb8f74a499ce48fb85a5606
Adithya Nair <adtya@adtya.xyz>
2025-01-25 19:45:56 +0530

    add gloria's new public key

--- common/users.nix
+++ common/users.nix
@@ -29,7 +29,7 @@
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxDgoV9yf+yPnp4pt5EWgo7uC25W66ehoL/rlshVW+8 Skipper"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFABsWbZoCVsi5fNBQjThYIZkAFwB6Q8WQpncdxE4iD Gloria"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDV12zea8VW/mttaCEat46epaM9VBxk0PZS5kf1l+iAi Gloria"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPodFFNUK16y9bjHVMhr+Ykro3v1FVLbmqKg7mjMv3Wz Kowalski"
       ];
     };
