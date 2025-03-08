c19042338821401686ae244e45bab92f8d062dec
Adithya Nair <adtya@adtya.xyz>
2025-03-09 02:59:06 +0530

    keep data mounted

--- hosts/layne/hardware/filesystem.nix
+++ hosts/layne/hardware/filesystem.nix
@@ -7,34 +7,34 @@ _: {
     "/" = {
       device = "/dev/disk/by-partlabel/LAYNE_ROOT";
       fsType = "btrfs";
-      options = [ "subvol=@root" "compress-force=zstd" "noatime" ];
+      options = [ "subvol=@root" "compress=zstd" "noatime" ];
       neededForBoot = true;
     };
 
     "/nix" = {
       device = "/dev/disk/by-partlabel/LAYNE_ROOT";
       fsType = "btrfs";
-      options = [ "subvol=@nix" "compress-force=zstd" "noatime" ];
+      options = [ "subvol=@nix" "compress=zstd" "noatime" ];
       neededForBoot = true;
     };
 
     "/persist" = {
       device = "/dev/disk/by-partlabel/LAYNE_ROOT";
       fsType = "btrfs";
-      options = [ "subvol=@persist" "compress-force=zstd" "noatime" ];
+      options = [ "subvol=@persist" "compress=zstd" "noatime" ];
       neededForBoot = true;
     };
 
     "/mnt/system" = {
       device = "/dev/disk/by-partlabel/LAYNE_ROOT";
       fsType = "btrfs";
-      options = [ "subvol=/" "compress-force=zstd" "noatime" ];
+      options = [ "subvol=/" "compress=zstd" "noatime" ];
     };
 
     "/mnt/data" = {
       device = "/dev/disk/by-uuid/524bc3e3-d2be-4f56-b124-1ef1ef3afa81";
       fsType = "btrfs";
-      options = [ "subvol=/" "compress-force=zstd" "noatime" "nofail" "x-systemd.automount" "x-systemd.device-timeout=5" ];
+      options = [ "subvol=/" "compress=zstd" "noatime" ];
     };
 
     "/boot" = {
