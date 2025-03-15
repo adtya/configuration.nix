6daf0da221f3ccb36bc952c720c0c0311f284efc
Adithya Nair <adtya@adtya.xyz>
2025-03-15 11:19:57 +0530

    cleanup /proc/cmdline

--- hosts/skipper/boot/default.nix
+++ hosts/skipper/boot/default.nix
@@ -12,14 +12,7 @@
       systemd.enable = true;
       verbose = false;
     };
-    kernelParams = [
-      "quiet"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
+    kernelParams = [ "quiet" ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
       "vm.dirty_ratio" = 3;
@@ -27,7 +20,6 @@
     loader = {
       efi.canTouchEfiVariables = true;
       systemd-boot.enable = lib.mkForce false;
-      timeout = 0;
     };
     lanzaboote = {
       enable = true;
