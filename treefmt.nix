29b70d3b1f310df82d13afe3cdf5601b0ee2253d
Adithya Nair <adtya@adtya.xyz>
2025-03-16 20:05:53 +0530

    setup treefmt-nix

--- treefmt.nix
+++ treefmt.nix
@@ -1,10 +1,27 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   projectRootFile = "flake.nix";
   programs = {
+    shellcheck.enable = true;
+    shfmt.enable = true;
     deadnix = {
       enable = true;
       no-lambda-pattern-names = true;
     };
-    nixfmt.enable = true;
+    statix.enable = true;
+    nixfmt = {
+      enable = true;
+      strict = true;
+    };
+    yamlfmt.enable = true;
+    prettier.enable = true;
+  };
+  settings.global = {
+    excludes = [
+      ".envrc"
+      "LICENSE"
+      "README.md"
+      "*.png"
+    ];
   };
 }
