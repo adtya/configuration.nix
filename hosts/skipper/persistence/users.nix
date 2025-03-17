2db2b220cb62ece76f000e7afd9c75b93ce08a01
Adithya Nair <adtya@adtya.xyz>
2025-03-17 22:56:07 +0530

    persist more stuff in ~

--- hosts/skipper/persistence/users.nix
+++ hosts/skipper/persistence/users.nix
@@ -10,12 +10,32 @@
         "Pictures"
         "Videos"
 
+        ".fly"
+        ".gnupg"
         ".mozilla"
         ".ssh"
-        ".gnupg"
 
-	".local/state"
+        ".config/1Password"
+        ".config/doctl"
+        ".config/gh"
+        ".config/hcloud"
+        ".config/lazygit"
+        ".config/nvim"
+        ".config/transmission-daemon"
+        ".config/transmission-remote-gtk"
+
+        ".local/share/direnv"
+        ".local/share/keyrings"
+        ".local/share/nvim"
+        ".local/share/nix"
+        ".local/share/systemd"
+        ".local/share/TelegramDesktop"
+        ".local/share/zsh"
+        ".local/share/zoxide"
+
+        ".local/state"
       ];
+      files = [ ".config/wallpaper_config.json" ];
     };
   };
 }
