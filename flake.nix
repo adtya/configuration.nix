a4dceb5cada5d5db70e2b1391988302d711cbb61
Adithya Nair <adtya@adtya.xyz>
2025-03-23 13:33:50 +0530

    remove redundant cache.nixos.org

--- flake.nix
+++ flake.nix
@@ -5,12 +5,10 @@
     extra-substituters = [
       "https://adtya.cachix.org"
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
     ];
     extra-trusted-public-keys = [
       "adtya.cachix.org-1:lAuNLx0Ehzx6FoH20rVkMD7KyZZevlLfvm3lwMAzrnU="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
