8f6ec949b567154b5f5da2f4cfde8ca21ec6e51d
Adithya Nair <adtya@adtya.xyz>
2025-04-09 23:04:51 +0530

    refactor

--- packages.nix
+++ packages.nix
@@ -1,10 +1,10 @@
 { pkgs, nixpkgs }:
 let
-  nixos-minimal = pkgs.nixos {
+  nixos-minimal-do = pkgs.nixos {
     imports = [ "${nixpkgs}/nixos/modules/virtualisation/digital-ocean-image.nix" ];
     system.stateVersion = "24.11";
   };
 in
 {
-  inherit (nixos-minimal) digitalOceanImage;
+  inherit (nixos-minimal-do) digitalOceanImage;
 }
