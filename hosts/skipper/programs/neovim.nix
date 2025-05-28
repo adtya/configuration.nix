3cc61de78f84033a7bf002401a41e54fc953d3e6
Adithya Nair <adtya@adtya.xyz>
2025-05-28 14:09:44 +0530

    use neovim nightly os-wide

--- hosts/skipper/programs/neovim.nix
+++ hosts/skipper/programs/neovim.nix
@@ -1,6 +1,10 @@
-_: {
+{ inputs, pkgs, ... }:
+{
   programs.neovim = {
     enable = true;
+    package = inputs.neovim-nightly.packages.${pkgs.system}.default;
     defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
   };
 }
