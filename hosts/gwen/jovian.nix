45e132db68d02eaa09dddb8eedb29e4f4cb37f31
Adithya Nair <adtya@adtya.xyz>
2025-05-29 00:48:52 +0530

    update jovian config

--- hosts/gwen/jovian.nix
+++ hosts/gwen/jovian.nix
@@ -1,8 +1,12 @@
-_: {
+{ primary-user, ... }:
+{
   jovian = {
+    devices.steamdeck.enable = true;
+    #hardware.has.amd.gpu = true;
     steam = {
       enable = true;
       autoStart = true;
+      user = primary-user.name;
     };
   };
 }
