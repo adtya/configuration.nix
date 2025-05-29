f73d87a57db369241d6787a8350452c4d9358d2e
Adithya Nair <adtya@adtya.xyz>
2025-05-29 22:00:31 +0530

    disable plymouth

--- hosts/gwen/boot/plymouth.nix
+++ hosts/gwen/boot/plymouth.nix
@@ -4,7 +4,7 @@ let
 in
 {
   boot.plymouth = {
-    enable = true;
+    enable = false;
     themePackages = lib.mkDefault [
       (pkgs.adi1090x-plymouth-themes.override { selected_themes = [ theme ]; })
     ];
