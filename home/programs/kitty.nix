cdb8b918bae23b1aeb5ba31b54b64138ab6b10d1
Adithya Nair <adtya@adtya.xyz>
2025-06-07 01:06:11 +0530

    flying cursors weeee

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -14,6 +14,9 @@
       window_padding_width = "4.0";
       background_opacity = "0.98";
       cursor_shape = "beam";
+      cursor_trail = 2;
+      cursor_trail_decay = "0.1 0.2";
+      cursor_trail_start_threshold = 3;
       scrollback_lines = 2000;
       copy_on_select = "clipboard";
       url_style = "curly";
