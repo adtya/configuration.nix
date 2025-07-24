c2d74a7d0b41a732cdc2dd47f8731012220d7bf3
Adithya Nair <adtya@adtya.xyz>
2025-07-24 21:52:15 +0530

    add thor to ssh authorized keys

--- hosts/shared/users.nix
+++ hosts/shared/users.nix
@@ -22,8 +22,9 @@
       inherit (primary-user) name long-name;
       password-hash-file = config.sops.secrets."passwd/${primary-user.name}".path;
       allowed-ssh-keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxDgoV9yf+yPnp4pt5EWgo7uC25W66ehoL/rlshVW+8 skipper"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPxDgoV9yf+yPnp4pt5EWgo7uC25W66ehoL/rlshVW+8 Skipper"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPodFFNUK16y9bjHVMhr+Ykro3v1FVLbmqKg7mjMv3Wz Kowalski"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKxjkWuf73U2AfJajJfNl6h4/R5ko+WCI1nl9XH/9AJP Thor"
       ];
     };
   };
