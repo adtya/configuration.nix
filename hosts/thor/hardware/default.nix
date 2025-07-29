f2f66ff2f7884f6cf6eea0204ad127544703bbf2
Adithya Nair <adtya@adtya.xyz>
2025-07-29 21:10:39 +0530

    rocm stuff

--- hosts/thor/hardware/default.nix
+++ hosts/thor/hardware/default.nix
@@ -27,7 +27,18 @@
   };
 
   environment.sessionVariables.VDPAU_DRIVER = "radeonsi";
-  systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
+  systemd.tmpfiles.rules =
+    let
+      rocmEnv = pkgs.symlinkJoin {
+        name = "rocm-combined";
+        paths = with pkgs.rocmPackages; [
+          rocblas
+          hipblas
+          clr
+        ];
+      };
+    in
+    [ "L+    /opt/rocm   -    -    -     -    ${rocmEnv}" ];
 
   hardware = {
     amdgpu = {
