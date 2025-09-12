2e0141bb170c243b284a0f622a9c92052a8432ef
Adithya Nair <adtya@adtya.xyz>
2025-09-12 23:49:21 +0530

    move package overrides to overlays

--- home/thor/programs/default.nix
+++ home/thor/programs/default.nix
@@ -3,13 +3,4 @@
   imports = [ ./gaming.nix ];
 
   home.packages = [ pkgs.piper ];
-
-  programs.btop.package = pkgs.btop-rocm.overrideAttrs (_oldAttrs: {
-    src = pkgs.fetchFromGitHub {
-      owner = "aristocratos";
-      repo = "btop";
-      rev = "4f5abbb3ebf6c5520ed33cf9f81bed405b6a0b4b";
-      hash = "sha256-ih66RmZtui53DgFmiKcy08ibs0KXRCKFURD8Kh3oYlo=";
-    };
-  });
 }
--- packages/default.nix
+++ packages/default.nix
@@ -3,4 +3,23 @@ _final: prev: {
   getpaper = prev.callPackage ./scripts/getpaper { };
   rofi-bluetooth = prev.callPackage ./rofi-bluetooth { };
   smc-nupuram = prev.callPackage ./smc-nupuram { };
+
+  # package overrides
+  btop = prev.btop.overrideAttrs (_oldAttrs: {
+    src = prev.fetchFromGitHub {
+      owner = "aristocratos";
+      repo = "btop";
+      rev = "bdddfc46a2bbfe4ba70c7fba4fbb30978d61dab9";
+      hash = "sha256-2SGQLSyGMg3Fv10ewu8b64Z08fBbAkVCvEj2+0GGrHI=";
+    };
+  });
+  gamemode = prev.gamemode.overrideAttrs (_oldAttrs: {
+    version = "2025-09-04";
+    src = prev.fetchFromGitHub {
+      owner = "FeralInteractive";
+      repo = "gamemode";
+      rev = "f0a569a5199974751a4a75ebdc41c8f0b8e4c909";
+      hash = "sha256-9DB8iWiyrM4EJ94ERC5SE9acrhqeI00BF1wU0umeNFg=";
+    };
+  });
 }
