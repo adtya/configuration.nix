d99424340bbc8b5466eb0f7ee302b6a8aed4f16a
Adithya Nair <adtya@adtya.xyz>
2025-09-15 19:19:37 +0530

    fix warning during ssh config eval

--- home/common/programs/ssh.nix
+++ home/common/programs/ssh.nix
@@ -1,8 +1,16 @@
 _: {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks."*" = {
       compression = true;
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      hashKnownHosts = true;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      controlPath = "~/.ssh/master-%r@%n:%p";
     };
   };
 }
