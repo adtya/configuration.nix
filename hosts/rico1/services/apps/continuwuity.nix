48eab16ba6af56dae12136102a0227184c9ec78e
Adithya Nair <adtya@adtya.xyz>
2025-09-20 07:50:22 +0530

    configure continuwuity

--- hosts/rico1/services/apps/continuwuity.nix
+++ hosts/rico1/services/apps/continuwuity.nix
@@ -18,7 +18,15 @@
         allow_registration = true;
         registration_token_file = config.sops.secrets."continuwuity/registration_token".path;
 
+        new_user_displayname_suffix = "🔥";
         require_auth_for_profile_requests = true;
+        allow_device_name_federation = true;
+        trusted_servers = [
+          "matrix.org"
+          "poddery.com"
+          "frei.chat"
+        ];
+        query_trusted_key_servers_first = false;
 
         well_known = {
           client = "https://matrix.ironyofprivacy.org";
