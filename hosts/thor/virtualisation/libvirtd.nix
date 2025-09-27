d1e13ad85d018b8c710af61b6396ebf5d9b9af1b
Adithya Nair <adtya@adtya.xyz>
2025-09-27 19:30:55 +0530

    ignore virtual network interfaces

--- hosts/thor/virtualisation/libvirtd.nix
+++ hosts/thor/virtualisation/libvirtd.nix
@@ -13,4 +13,34 @@
     };
   };
   nodeconfig.users.primary.extra-groups = [ "libvirtd" ];
+
+  systemd.network.networks = {
+    "20-virbr" = {
+      matchConfig = {
+        Name = "virbr*";
+        Type = "bridge";
+      };
+      linkConfig = {
+        Unmanaged = true;
+      };
+    };
+    "22-veth" = {
+      matchConfig = {
+        Name = "veth*";
+        Type = "ether";
+      };
+      linkConfig = {
+        Unmanaged = true;
+      };
+    };
+    "23-vnet" = {
+      matchConfig = {
+        Name = "vnet*";
+        Type = "ether";
+      };
+      linkConfig = {
+        Unmanaged = true;
+      };
+    };
+  };
 }
