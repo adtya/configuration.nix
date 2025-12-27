823a80eece4ef869d20eef79f63d9bc4db104caf
Adithya Nair <adtya@adtya.xyz>
2025-12-27 22:04:49 +0530

    cleanup substituters

--- modules/nix.nix
+++ modules/nix.nix
@@ -85,23 +85,9 @@ in
     (lib.mkIf cfg.trust-wheel { nix.settings.trusted-users = [ "@wheel" ]; })
     (lib.mkIf cfg.enable-extra-substituters {
       nix.settings = {
-        substituters = [
-          "https://adtya.cachix.org"
-          "https://cache.soopy.moe"
-          "https://nix-community.cachix.org"
-          "https://cache.nixos.org"
-        ];
-        trusted-substituters = [
-          "https://adtya.cachix.org"
-          "https://cache.soopy.moe"
-          "https://nix-community.cachix.org"
-          "https://cache.nixos.org"
-        ];
-        trusted-public-keys = [
-          "adtya.cachix.org-1:lAuNLx0Ehzx6FoH20rVkMD7KyZZevlLfvm3lwMAzrnU="
-          "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo="
+        extra-substituters = [ "https://nix-community.cachix.org" ];
+        extra-trusted-public-keys = [
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
     })
